733ae4ef3690e4c319bef376d386115f83113f09
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-05-06 23:48:40 +0300

    setting design for latest news - gallery section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,2 @@
-//Add a toggle effect on navbar list items
-
-function toggleActive(element) {
-
-  const navItems = document.querySelectorAll('.nav-item');
-  navItems.forEach(item => item.classList.remove('active'));
-
- 
-  element.classList.add('active');
-}
 
 
