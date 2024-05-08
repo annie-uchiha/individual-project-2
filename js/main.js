c8d4178dc3af4630f00129c004acbf6da59abaf4
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-05-08 23:06:06 +0300

    add js functionality show more - show less btn - gallery section

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("#toggleBtn").click(function () {
+    $(".additional-images").toggle();
+    $(this).text(function (i, text) {
+      return text === "SHOW MORE" ? "SHOW LESS" : "SHOW MORE";
+    });
+
+    // Apply CSS styles to the additional images
+    if ($(".additional-images").is(":visible")) {
+      $(".gallery-section__group2img").css({
+        display: "flex",
+        "flex-direction": "row",
+        "justify-content": "center",
+        margin: "10px",
+      });
+    }
+  });
+});
