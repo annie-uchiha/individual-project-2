b1d6f972e64b85df96b23fe746e4650dcd78615b
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-05-10 00:52:42 +0300

    styling the slick slider

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true,
+    prevArrow:
+      '<button class="slick-prev" aria-label="Previous" type="button"><i class="fa-solid fa-circle-arrow-left"></i></button>',
+    nextArrow:
+      '<button class="slick-next" aria-label="Next" type="button"><i class="fa-solid fa-circle-arrow-right"></i></button>',
     responsive: [
       {
         breakpoint: 768,
